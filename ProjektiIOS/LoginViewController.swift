df5a02c15072b147f92f3449b9da0d8703ff55d5
Festim Krasniqi <106912999+FestimKrasniqi@users.noreply.github.com>
2024-02-23 20:44:33 +0100

    Initial Commit

---
+++ ProjektiIOS/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProjektiIOS
+//
+//  Created by R-Tech on 23.2.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
