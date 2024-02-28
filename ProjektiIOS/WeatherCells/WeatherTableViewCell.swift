cf65bd14dcc26fabd31d243942b57ae03a37be25
Festim Krasniqi <106912999+FestimKrasniqi@users.noreply.github.com>
2024-02-28 16:04:18 +0100

    Added functionality for json decoder and added the initial design for weathertableviewcell

--- ProjektiIOS/WeatherCells/WeatherTableViewCell.swift
+++ ProjektiIOS/WeatherCells/WeatherTableViewCell.swift
@@ -8,10 +8,16 @@
 import UIKit
 
 class WeatherTableViewCell: UITableViewCell {
+    
+    @IBOutlet var dayLabel:UILabel!
+    @IBOutlet var highTempLabel:UILabel!
+    @IBOutlet var lowTempLabel:UILabel!
+    @IBOutlet var iconImage:UIImageView!
+    
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        backgroundColor = .gray
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -26,5 +32,35 @@ class WeatherTableViewCell: UITableViewCell {
         return UINib(nibName: "WeatherTableViewCell", bundle: nil)
     }
 
+    func configure(with model:WeatherInfo) {
+        self.lowTempLabel.text = "\(Int(model.main.tempMin - 273.15))º"
+        self.highTempLabel.text = "\(Int(model.main.tempMax - 273.15))º"
+        self.iconImage.image = UIImage(named: "Sun")
+        self.iconImage.contentMode = .scaleAspectFit
+        if let date = getDateFromDateString(model.dtTxt) {
+                // Format the date as required
+                self.dayLabel.text = getDayForDate(date)
+            } else {
+                self.dayLabel.text = ""
+            }
+        
+    }
+    
+    func getDateFromDateString(_ dateString: String) -> Date? {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        return formatter.date(from: dateString)
+    }
+
+    func getDayForDate(_ date: Date?) -> String {
+        guard let inputDate = date else {
+            return ""
+        }
+
+        let formatter = DateFormatter()
+        formatter.dateFormat = "EEEE" // Monday
+        return formatter.string(from: inputDate)
+    }
+  
     
 }
--- ProjektiIOS/WeatherViewController.swift
+++ ProjektiIOS/WeatherViewController.swift
@@ -12,7 +12,7 @@ class WeatherViewController: UIViewController,UITableViewDelegate,UITableViewDat
     
     @IBOutlet var table:UITableView!
     
-    var models = [Weather] ()
+    var models = [WeatherInfo] ()
     
     var currentLocation:CLLocation?
     
@@ -20,6 +20,7 @@ class WeatherViewController: UIViewController,UITableViewDelegate,UITableViewDat
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         table.register(HourlyTableViewCell.nib(), forCellReuseIdentifier: HourlyTableViewCell.identifier)
         
         table.register(WeatherTableViewCell.nib(), forCellReuseIdentifier: WeatherTableViewCell.identifier)
@@ -49,31 +50,45 @@ class WeatherViewController: UIViewController,UITableViewDelegate,UITableViewDat
     
     func requestWeatherForLocation () {
         guard let currentLocation = currentLocation else {
-            return
-        }
-        let long = currentLocation.coordinate.longitude
-        let lat = currentLocation.coordinate.latitude
-        
-        print(lat)
-        print(long)
-        
-        let apiKey = "d422dc0f81e96ccfe7ce31a35819b0df"
-        let urlString = "https://api.openweathermap.org/data/2.5/weather?&appid=\(apiKey)\(lat),\(long)"
-        
-        
+               return
+           }
+           
+           let lon = currentLocation.coordinate.longitude
+           let lat = currentLocation.coordinate.latitude
+           
+           let urlString = "https://api.openweathermap.org/data/2.5/forecast?lat=\(lat)&lon=\(lon)&appid=d422dc0f81e96ccfe7ce31a35819b0df"
+           
+           guard let url = URL(string: urlString) else {
+               print("Invalid URL")
+               return
+           }
 
-        URLSession.shared.dataTask(with: URL(string:urlString)!, completionHandler: {
-            data,response,error in
-            
-            guard let data = data,error == nil else {
-                print("Something went wrong")
-                return
-            }
+           let session = URLSession(configuration: .default)
+           let task = session.dataTask(with: url) { data, response, error in
+               guard let data = data else {
+                   if let error = error {
+                       print("Error: \(error)")
+                   }
+                   return
+               }
+               
+               do {
+                   let decoder = JSONDecoder()
+                
+                   let weatherData = try decoder.decode(WeatherData.self, from: data)
+                   print(weatherData.list)
+                   DispatchQueue.main.async {
+                    self.models.append(contentsOf: weatherData.list)
+                    self.table.reloadData()
+                   }
+               } catch {
+                   print("Error decoding JSON: \(error)")
+               }
+           }
+        task.resume()
+       }
 
-            
-            })
-
-    }
+    
 
     
     
@@ -83,11 +98,102 @@ class WeatherViewController: UIViewController,UITableViewDelegate,UITableViewDat
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: WeatherTableViewCell.identifier,for:indexPath) as! WeatherTableViewCell
+        cell.configure(with: models[indexPath.row])
+        return cell
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
     }
   
 }
 
-struct Weather {
-    
+
+
+struct WeatherData: Codable {
+    let cod: String
+    let message:Int
+    let cnt: Int
+    let list: [WeatherInfo]
+    let city: CityInfo
 }
+
+struct WeatherInfo: Codable {
+    let dt: Int
+    let main: MainInfo
+    let weather: [WeatherDetail]
+    let clouds: CloudInfo
+    let wind: WindInfo
+    let visibility: Int
+    let pop: Double
+    let sys: SysInfo
+    let dtTxt: String
+
+    private enum CodingKeys: String, CodingKey {
+        case dt, main, weather, clouds, wind, visibility, pop, sys
+        case dtTxt = "dt_txt"
+    }
+}
+
+struct MainInfo: Codable {
+    let temp: Double
+    let feelsLike: Double
+    let tempMin: Double
+    let tempMax: Double
+    let pressure: Int
+    let seaLevel: Int
+    let groundLevel: Int
+    let humidity: Int
+    let tempKf: Double
+
+    private enum CodingKeys: String, CodingKey {
+        case temp
+        case feelsLike = "feels_like"
+        case tempMin = "temp_min"
+        case tempMax = "temp_max"
+        case pressure
+        case seaLevel = "sea_level"
+        case groundLevel = "grnd_level"
+        case humidity
+        case tempKf = "temp_kf"
+    }
+}
+
+struct WeatherDetail: Codable {
+    let id: Int
+    let main: String
+    let description: String
+    let icon: String
+}
+
+struct CloudInfo: Codable {
+    let all: Int
+}
+
+struct WindInfo: Codable {
+    let speed: Double
+    let deg: Int
+    let gust: Double
+}
+
+struct SysInfo: Codable {
+    let pod: String
+}
+
+struct CityInfo: Codable {
+    let id: Int
+    let name: String
+    let coord: CoordInfo
+    let country: String
+    let population: Int
+    let timezone: Int
+    let sunrise: Int
+    let sunset: Int
+}
+
+struct CoordInfo: Codable {
+    let lat: Double
+    let lon: Double
+}
+
+
